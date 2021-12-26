4ba9a7b86348d0fde0817e39cf098a5f768ace74
Mikko Peltonen <mikko.peltonen@reaktor.com>
2021-12-26 16:44:14 +0200

    Remove experimental state handling code

--- src/map_matching/client/pages.cljs
+++ src/map_matching/client/pages.cljs
@@ -1,26 +1,14 @@
 (ns map-matching.client.pages
   (:require-macros [cljs.core.async.macros :refer [go]])
-  (:require [reagent.core :as reagent]
-            ["react" :as react]
+  (:require ["react" :as react]
             [map-matching.client.ol-map :as ol-map]
             [cljs.core.async :refer [<!]]
             [map-matching.client.api-client :as api-client]))
 
-(defonce tm (reagent/atom ""))
-
-(defn get-time []
-  (go (let [resp (<! (api-client/get-time))]
-        (reset! tm (:time (:body resp))))))
-
 (def map-element-id "map")
 
 (defn index []
-  (let [[count set-count] (react/useState 0)]
-    (react/useEffect #(ol-map/setup-map map-element-id) #js [])
-    [:div
-     [:p (str "Count: " count)]
-     [:p (str "Time: " @tm)]
-     [:button {:on-click #(set-count (inc count))} "Inc count"]
-     [:button {:on-click get-time} "Get time"]
-     [:button {:on-click ol-map/center-map } "Reset view"]
-     [:div {:id map-element-id :style {:height "100vh" :width "100vw"}}]]))
+  (react/useEffect #(ol-map/setup-map map-element-id) #js [])
+  [:div
+    [:button {:on-click ol-map/center-map } "Reset view"]
+    [:div {:id map-element-id :style {:height "100vh" :width "100vw"}}]])
