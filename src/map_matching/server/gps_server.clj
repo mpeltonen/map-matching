978bb6c9dd99cc0f6ace8323999f5aaec69f21e0
Mikko Peltonen <mikko.peltonen@reaktor.com>
2022-01-02 21:20:56 +0200

    Initial GPS server to receive location updates
    Supports Teltonika TCP protocol.

---
+++ src/map_matching/server/gps_server.clj
@@ -0,0 +1,79 @@
+(ns map-matching.server.gps-server
+  (:require
+    [clojure.java.io :as io])
+  (:import [java.net InetAddress ServerSocket Socket SocketException]
+           [java.io DataInputStream DataOutputStream]
+           [java.util Date]))
+
+(defn read-unsigned [stream length]
+  (let [bytes (byte-array length)]
+    (.read stream bytes 0 length)
+    (->> bytes
+      (map (partial format "%02x"))
+      (apply (partial str "0x"))
+      (read-string))))
+
+(defn handle-imei-message [imei-length in-stream out-stream]
+  (let [imei-buf (byte-array imei-length)
+        _ (.read in-stream imei-buf 0 imei-length)
+        imei (String. imei-buf)]
+    (println "Received offer for IMEI" (format "%s," imei) "accepting")
+    (.writeByte out-stream 1)
+    (.flush out-stream)))
+
+(defn handle-avl-message [in-stream out-stream]
+  (let [msg-length (read-unsigned in-stream 4)
+        codec-type (.readUnsignedByte in-stream)
+        num-records-1 (.readUnsignedByte in-stream)
+        ts (read-unsigned in-stream 8)
+        priority (.readUnsignedByte in-stream)
+        lon (double (/ (read-unsigned in-stream 4) 1e7))
+        lat (double (/ (read-unsigned in-stream 4) 1e7))
+        _ (.skipBytes in-stream 5)
+        speed (.readUnsignedShort in-stream)]
+    (println "Received AVL message for codec" (format "0x%02x," codec-type) "length" msg-length "bytes," num-records-1 "data record(s)")
+    (println "Time:" (str (Date. ts)) "priority:" priority "latitude:" lat "longitude:" lon "speed:" speed)
+    (.write out-stream (byte-array [0 0 0 num-records-1]) 0 4)
+    (.flush out-stream)))
+
+(defn handle-tcp-connection [socket]
+  (let [in-stream (DataInputStream. (io/input-stream socket))
+        out-stream (DataOutputStream. (io/output-stream socket))]
+    (while (not (.isClosed socket))
+      ;; See https://wiki.teltonika-gps.com/view/Teltonika_Data_Sending_Protocols,
+      ;; "Communication with server" section(s).
+      (let [preamble16 (.readUnsignedShort in-stream)]
+        (if (> preamble16 0)
+          (do
+            (handle-imei-message preamble16 in-stream out-stream))
+          (do
+            (.skipBytes in-stream 2)
+            (handle-avl-message in-stream out-stream)
+            (.close socket)))))))
+
+(defn finalize-connection [socket]
+  (let [remote-addr (str (.getRemoteSocketAddress socket))]
+    (when-not (.isClosed socket)
+      (.close socket))
+    (println "Connection from" remote-addr "closed")))
+
+(defn accept-connection [server-socket]
+  (let [socket (.accept server-socket)]
+    (println "New connection from" (str (.getRemoteSocketAddress socket)))
+    (future
+      (try (handle-tcp-connection socket)
+           (finally
+             (finalize-connection socket))))))
+
+(defn running? [server-socket]
+  (not (.isClosed server-socket)))
+
+(defn start-gps-server []
+  (let [server-socket (ServerSocket. 7001 10 (InetAddress/getByName "127.0.0.1"))]
+    (future
+      (while (running? server-socket)
+        (try
+          (accept-connection server-socket)
+          (catch SocketException e
+            (println (str "caught exception: " (.toString e)))))))
+    (println "Started TCP server on port 7001")))
--- src/map_matching/server/main.clj
+++ src/map_matching/server/main.clj
@@ -1,7 +1,9 @@
 (ns map-matching.server.main
   (:require [ring.adapter.jetty :as jetty]
+            [map-matching.server.gps-server :refer [start-gps-server]]
             [map-matching.server.routing :refer [router]])
   (:gen-class))
 
 (defn -main []
+  (start-gps-server)
   (jetty/run-jetty #'router {:port 3001 :join? false}))
