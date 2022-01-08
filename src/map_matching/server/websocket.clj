06d35e05d23bf4d34d1e7828281148020d61b120
Mikko Peltonen <mikko.peltonen@reaktor.com>
2022-01-08 21:56:04 +0200

    Send app level ws ping every 2 mins
    To prevent Jetty WebSocket platform idle timeout.

--- src/map_matching/server/websocket.clj
+++ src/map_matching/server/websocket.clj
@@ -1,9 +1,25 @@
 (ns map-matching.server.websocket
   (:require
     [ring.util.response :refer [response]]
-    [ring.adapter.jetty9 :as jetty]))
+    [ring.adapter.jetty9 :as jetty]
+    [clojure.pprint :as pp]
+    [chime.core :refer [chime-at periodic-seq]])
+  (:import [java.time Instant Duration]
+           [java.util Date]))
 
 (defonce ws-clients (atom #{}))
+
+(defn every-2-mins []
+  (-> (periodic-seq (Instant/now) (Duration/ofMinutes 2)) rest))
+
+(defn broadcast-msg [msg]
+  (doseq [ws @ws-clients]
+    (jetty/send! ws msg)))
+
+(future
+  (chime-at (every-2-mins)
+    (fn [t]
+      (broadcast-msg (with-out-str (pp/pprint {:ping (str t)}))))))
 
 (defn- on-connect [ws]
   (tap> [:ws :connect (str (jetty/remote-addr ws))])
